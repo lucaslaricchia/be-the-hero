040c7dac4c687280f05276de4819468d2481069a
--global <lucaslaricchia@gmail.com>
2020-09-13 22:24:31 -0300

    frontend web finalizado

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi'
 import './styles.css';
+import api from '../../services/api';
+
+
 
 export default function Logon(){
+
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    function handleLogin(e){
+        e.preventDefault();
+
+        try{
+            const response = api.post('sessions', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+            history.push('/profile');
+        } catch(err){
+            alert('Falha no login, tente novamente');
+        }
+
+    }
+
+
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src="" alt="Be the hero"/>
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
-                    <input placeholder="Sua ID" />
+                    <input 
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
                     <button className="button" type="submit">Entrar</button>
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041"/>
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
 
